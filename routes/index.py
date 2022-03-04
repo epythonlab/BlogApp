565c6762126d5e0043f7cdd61b081626de94d28d
Dejene Techane <dtechane6@gmail.com>
2022-03-04 02:20:56 -0500

    Update and delete post added

--- routes/index.py
+++ routes/index.py
@@ -36,3 +36,37 @@ def create_post():
         # flash message
         flash("Post successfuly created!")
         return redirect(url_for('post'))
+
+# delete speficied post
+@app.route('/delete/<int:id>', methods=['POST', 'GET'])
+def delete(id):
+    if request.method=='GET':
+        deleted_post = Post.query.get(id)
+        db.session.delete(deleted_post)
+        db.session.commit()
+        flash("Post successfuly deleted!")
+        return redirect(url_for('index'))
+
+# edit post
+@app.route('/edit/<int:id>', methods=['POST', 'GET'])
+def edit(id):
+    if request.method=='GET':
+
+        # get a specified post by id
+        post_data = Post.query.get(id)
+
+        return render_template('edit.html', edit_post=post_data)
+
+# update specified post
+@app.route('/update/', methods=['POST', 'GET'])
+def update():
+    if request.method=='POST':
+        # get post
+        update_post = Post.query.get(request.form['id'])
+
+        #update post
+        update_post.title = request.form['title']
+        update_post.desc = request.form['desc']
+        db.session.commit()
+        flash("Post updated!")
+        return redirect(url_for('index'))
